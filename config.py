4cf8a81beb11a13d27ca84392f0e85f8c98fff40
OrewaSanjii-Dono <sanjihunbechutiye+new56@gmail.com>
2025-01-18 03:11:31 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -7,34 +7,34 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather, --⚠️ REQUIRED--
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7614243529:AAG-0PG3d2dUByx0V3kyNclugjaQTaQvpAU")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7045740764:AAGPFItTniVf2Djr8zOB2_oaZVDQh1wwaog")
 
 #Your API ID from my.telegram.org, --⚠️ REQUIRED--
-APP_ID = int(os.environ.get("APP_ID", "20718334"))
+APP_ID = int(os.environ.get("APP_ID", "24371796"))
 
 #Your API Hash from my.telegram.org, --⚠️ REQUIRED--
-API_HASH = os.environ.get("API_HASH", "4e81464b29d79c58d0ad8a0c55ece4a5")
+API_HASH = os.environ.get("API_HASH", "8121c78f4b8b31e88cc2623d1277338d")
 
 #Your db channel Id --⚠️ REQUIRED--
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002338551452"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002016717904"))
 
 #OWNER ID --⚠️ REQUIRED--
-OWNER_ID = int(os.environ.get("OWNER_ID", "8160777407"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1439890119"))
 
 #SUPPORT_GROUP: This is used for normal users for getting help if they don't understand how to use the bot --⚠ OPTIONAL--
-SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "-1002368823802")
+SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "-1002097138409")
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #Database --⚠️ REQUIRED--
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://uffobitoxe:umaid2008@cluster0.vpebe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://AnimeRavenBots:AnimeRavenBots@animeravenbots.huekk.mongodb.net/?retryWrites=true&w=majority&appName=HitokiriBattousaiBot")
+DB_NAME = os.environ.get("DATABASE_NAME", "HitokiriBattousaiBot")
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #Collection of pics for Bot // #Optional but atleast one pic link should be replaced if you don't want predefined links
-PICS = (os.environ.get("PICS", "https://envs.sh/omB.jpg")).split() #Required
+PICS = (os.environ.get("PICS", "https://envs.sh/H27.jpg")).split() #Required
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
